08bda57c946a01c93483b4cdbedbb2d9d7a6e641
Yasenn <12043095+yasenn@users.noreply.github.com>
2022-10-26 16:13:03 +0300

    clean-glossary-words.sh

--- text/clean-glossary-words.sh
+++ text/clean-glossary-words.sh
@@ -2,9 +2,7 @@
 # this script prints only words from file $1 that don't exist in file $2
 
 for WORD in $(cat ${1} | xargs); do 
-  if $(grep -i "${WORD}" ${2} > /dev/null); 
-  then ; 
-  else 
-    echo ${WORD}; 
-  fi  ; 
+  if  $(grep -i "${WORD}" ${2} > /dev/null)  ; then 
+    echo ${WORD}
+  fi
 done | pv -l
